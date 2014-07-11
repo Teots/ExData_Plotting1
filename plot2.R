dbb41b57eb378c938c80216cc0aa0c14acffddef
Teots <christian.wuertz@tu-berlin.de>
2014-07-11 09:03:34 +0200

    Completed the first script.

---
+++ plot2.R
@@ -0,0 +1,14 @@
+# Read file
+rawData <- read.csv2('../household_power_consumption.txt')
+
+# Convert to R date format
+rawData$Date <- as.Date(rawData$Date, format = '%d/%m/%Y')
+
+# Filter relevant data
+data <- rawData[rawData$Date >= as.Date('2007-02-01') & rawData$Date <= as.Date('2007-02-02'), ]
+
+# Convert to R time format
+data$Time <- strptime(data$Time, format = '%H:%M:%S')
+
+# Create histogram and store it
+png(filename='figure/plot1.png', width = 480, height = 480)
